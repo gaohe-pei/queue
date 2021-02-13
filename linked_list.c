b705f24f4415bcff3e9a3c946602e9e74bbff668
gaohe <gaohe2008@gmail.com>
2021-02-12 21:18:57 -0700

    add insert_at_head fun

--- linked_list.c
+++ linked_list.c
@@ -26,17 +26,20 @@ node_t *creat_new_node(int value)
     result->next = NULL;
     return result;
 }
+node_t *insert_at_head(node_t *head, node_t *node_to_insert)
+{
+    node_to_insert->next = head;
+    return node_to_insert;
+}
 int main()
 {
-    node_t *head, *tmp;
-    tmp = creat_new_node(23);
-    head = tmp;
-    tmp = creat_new_node(12);
-    tmp->next = head;
-    head = tmp;
-    tmp = creat_new_node(45);
-    tmp->next = head;
-    head = tmp;
+    node_t *head = NULL, *tmp;
+
+    for (int i = 0; i < 25; i++)
+    {
+        tmp = creat_new_node(i);
+        head = insert_at_head(head,tmp);
+    }
 
     printlist(head);
     return 0;
