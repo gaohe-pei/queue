a29b9a147b6aa677ac78cd0ed4b7aee335fa2268
gaohe <gaohe2008@gmail.com>
2021-02-14 10:07:35 -0700

    add find_node function add insert_after_node function

--- linked_list.c
+++ linked_list.c
@@ -26,11 +26,34 @@ node_t *creat_new_node(int value)
     result->next = NULL;
     return result;
 }
-node_t *insert_at_head(node_t *head, node_t *node_to_insert)
+node_t *insert_at_head(node_t **head, node_t *node_to_insert)
 {
-    node_to_insert->next = head;
+    node_to_insert->next = *head;
+    *head = node_to_insert;
+    //printf("%p-%p  ", *head, head);
     return node_to_insert;
 }
+
+void insert_after_node(node_t *node_to_insert_after, node_t *newnode)
+{
+    newnode->next = node_to_insert_after->next;
+    node_to_insert_after->next = newnode;
+}
+
+node_t *find_node(node_t *head, int value)
+{
+    node_t *tmp = head;
+    while (tmp != NULL)
+    {
+        if (tmp->value == value)
+        {
+            return tmp;
+        }
+        tmp = tmp->next;
+    }
+    return NULL;
+}
+
 int main()
 {
     node_t *head = NULL, *tmp;
@@ -38,9 +61,11 @@ int main()
     for (int i = 0; i < 25; i++)
     {
         tmp = creat_new_node(i);
-        head = insert_at_head(head,tmp);
+        insert_at_head(&head, tmp);
+        //printf("%p-%p\n", &head, head);
     }
-
+    tmp=find_node(head,15);
+    insert_after_node(tmp,creat_new_node(86));
     printlist(head);
     return 0;
 }
